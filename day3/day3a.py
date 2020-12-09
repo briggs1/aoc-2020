3ae2fc9fd11a2eedeaeb44d1e38e56ccd1e9d571
Britton Riggs <briggs@multiweb.com>
2020-12-09 17:06:59 -0500

    Day 3 Part 1

--- day3/day3a.py
+++ day3/day3a.py
@@ -5,7 +5,9 @@ i = 0
 with open(text_file) as f:
     data = f.read()
 
-while i < len(data.split("\n")):
+data = data.split("\n")
+
+while i < len(data):
     pos = (i * 3) % len(data[i])
     if data[i][pos] == "#":
         total_trees += 1

fd52c4c3a44b47a69c9d30b16232a87e06f16207
Britton Riggs <briggs@multiweb.com>
2020-12-12 22:04:11 -0500

    Finished Day 5 Part 1

---
+++ day5/day5a.py
@@ -0,0 +1,31 @@
+text_file = "day5/input.txt"
+rows = []
+columns = [i for i in range(8)]
+seat_ids = []
+
+with open(text_file) as f:
+    data = f.read().split("\n")
+
+for line in data:
+    rows = [i for i in range(128)]
+    for char in line[0:7]:
+
+        half = len(rows)//2
+
+        if char == "B":
+            rows = rows[half:]
+        elif char == "F":
+            rows = rows[:half]
+
+    for char in line[:3]:
+
+        half = len(columns)/2
+
+        if char == "R":
+            columns = columns[half:]
+        elif char == "L":
+            columns = columns[:half]
+
+    seat_ids = seat_ids + [rows[0] * 8 + columns[0]]
+
+print(max(seat_ids))
